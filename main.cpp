86c2bbcac390ce0c74870abce1ac4e95ef609da6
valenic-UP <0282748@up.edu.mx>
2026-02-25 14:32:56 -0600

    menu

--- main.cpp
+++ main.cpp
@@ -3,74 +3,64 @@ using namespace std;
 
 int main(){
 
-    int mes;
-    cout<<"Escribe un número del 1-12: ";
-    cin>>mes;
+    int opcion,n;
+    cout<<"escribe un número (n): ";
+    cin>>n;
+    
 
-    switch(mes){
-        case 1:{
-            cout<<"enero";
-            break;
-        }
-        case 2:{
-            cout<<"febrero";
-            break;
+    do{
+        cout<<"Elige una opción: (1-5)"<<endl;
+        cout<<"1. Pintar los pares hasta n"<<endl;
+        cout<<"2. Pintar los impares hasta n"<<endl;
+        cout<<"3. pinta los primeros n numeros diciendo cuales son pares y cuales impares"<<endl;
+        cout<<"4. La suma de "<<n<<" numeros"<<endl;
+        cout<<"5. Salir"<<endl;
+        cin>>opcion;
 
+        switch(opcion){
+            case 1:{
+                cout<<"los numeros pares son: ";
+                for(int i=0; i<=n; i+=2){
+                    cout<<i<<", ";
+                }
+                break;
+            }
+            case 2:{
+                cout<<"los numeros impares son: ";
+                for (int i=1; i<=n; i+=2)
+                cout<<i<<", ";
+                break;
+            }
+            case 3:{
+                for(int i=1; i<=n; i++){
+                    if (i%2==0){
+                        cout<<i<<": par"<<endl;
+                    }
+                    else{
+                        cout<<i<<": impar"<<endl;
+                    }
+                }
+                break;
+            }
+            case 4:{
+                int suma=0;
+                int num;
+                for (int i=0; i<n; i++){
+                    cout<<"escribe un número: ";
+                    cin>>num;
+                    suma=suma+num;
+                }
+                cout<<"la suma es de: "<<suma<<endl;
+                break;
+            }
+            case 5:{
+                break;
+            }
+            default:{
+                cout<<"ingresa una opcion del 1-5";
+            }
         }
-        case 3:{
-            cout<<"marzo";
-            break;
-
-        }
-        case 4:{
-            cout<<"abril";
-            break;
-
-        }
-        case 5:{
-            cout<<"mayo";
-            break;
-
-        }
-        case 6:{
-            cout<<"junio";
-            break;
-
-        }
-        case 7:{
-            cout<<"julio";
-            break;
-
-        }
-        case 8:{
-            cout<<"agosto";
-            break;
-
-        }
-        case 9:{
-            cout<<"septiembre";
-            break;
-
-        }
-        case 10:{
-            cout<<"octubre";
-            break;
-
-        }
-        case 11:{
-            cout<<"noviembre";
-            break;
-
-        }
-        case 12:{
-            cout<<"diciembre";
-            break;
-
-        }
-        default:{
-            cout<<"ingresa una opcion del 1-12";
-        }
-    }
+    }while(opcion!=5);
 
     return 0;
 }
